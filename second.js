3a14e3e9c1d1a314ec02ecf8839bae8c9c93226d
SathvaraDhruvin07 <sathvaradhruvin05@gmail.com>
2025-01-07 07:31:47 +0530

    first-commit

--- second.js
+++ second.js
@@ -1,45 +1,47 @@
 const container = document.querySelector(".container");
 const api = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=100";
 
-
 function pokemonData() {
-    container.textContent = "Loading data please wait...."
+    container.textContent = "Loading data please wait....";
     try {
-        fetch(api).then((response) => response.json())
+        fetch(api)
+            .then((response) => response.json())
             .then((data) => {
-                setTimeout(() => {
-                    let results = ""
-                    const pokemons = data.results;
-                    pokemons.forEach((items) => {
-                        fetch(items.url).then((res) => res.json())
-                            .then((details) => {
-                                const img = details.sprites.other.showdown.front_default;
-                                const name = details.name;
-                                const ablility = details.abilities[0].ability.name;
-                                console.log(ablility);
-                                results +=
-                                    `
-                               <div class="card">
+                let results = "";
+                const pokemons = data.results;
+                const promises = pokemons.map((items) =>
+                    fetch(items.url)
+                        .then((res) => res.json())
+                        .then((details) => {
+                            const img = details.sprites.other["official-artwork"].front_default;
+                            const name = details.name;
+                            const ability = details.abilities[0].ability.name;
+
+                            results += `
+                               <div class="card" data-aos="flip-right">
                                     <div class="card-inner">
-                                    <div class="card-front">
-                                    <img src="${img}" class="mt-5 mb-5 pt-5" alt="Pokemon" width="" height="" />
+                                        <div class="card-front">
+                                            <img src="${img}" alt="${name}" />
+                                            <span>${name}</span>
+                                        </div>
+                                        <div class="card-back">
+                                            <h5>Name: ${name}</h5>
+                                            <p>Ability: ${ability}</p>
+                                        </div>
                                     </div>
-                                    <div class="card-back">
-                                    <h5>Name:${name}</h5>
-                                    <p>Ability:  ${ablility}</p>
-                                    </div>
-                                    </div>
-                               </div>                      
-                            `
-                                    ;
-                                container.innerHTML = results;
-                            });
-                    });
+                               </div>`;
+                        })
+                );
+
+                Promise.all(promises).then(() => {
+                    container.innerHTML = results;
+
+                    AOS.refresh();
                 });
-            }, 3000);
+            });
     } catch (error) {
-        document.write("error");
-        console.log(error);
+        container.textContent = "An error occurred. Please try again.";
+        console.error(error);
     }
 }
 
